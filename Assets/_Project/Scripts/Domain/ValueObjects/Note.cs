b9263d4444c774559cb2ebcc09c77a1c871ed54d
HRkiller <killer1234790@gmail.com>
2025-10-04 15:40:27 +0300

    Add: added interface input service

---
+++ Assets/_Project/Scripts/Domain/ValueObjects/Note.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.Domain.ValueObjects
+{
+    public enum Note
+    {
+        
+    }
+}
---
+++ Assets/_Project/Scripts/Infrastructure/Input/IInputService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using _Project.Scripts.Domain.ValueObjects;
+using UnityEngine;
+
+namespace _Project.Scripts.Infrastructure.Input
+{
+    public interface IInputService
+    {
+        event Action CastApplied;
+        event Action<Note> NotePressed;
+        Vector3 MoveToPos { get; }
+    }
+}
