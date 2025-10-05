f109406d702c3e8d350e6764e71872e2f68a185e
HRkiller <killer1234790@gmail.com>
2025-10-05 19:27:07 +0300

    Add: added camera mover

---
+++ Assets/_Project/Scripts/Application/UseCases/Common/CameraMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Application.UseCases.Common
+{
+    public class CameraMover : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _offset;
+        [SerializeField] private float _speed;
+
+        [SerializeField] private Transform _camera;
+        [SerializeField] private Transform _target;
+
+        private void Update()
+        {
+            var target = new Vector3(_target.position.x, 0f, _target.position.z);
+            
+            _camera.position = Vector3.Lerp(_camera.transform.position, target + _offset, Time.deltaTime * _speed);    
+        }
+    }
+}
