09f9076a11405e8a602c0b36a9c43db8e1a7f0d2
HRkiller <killer1234790@gmail.com>
2025-10-05 19:06:56 +0300

    Add: added a player damager from enemies

---
+++ Assets/_Project/Scripts/Application/UseCases/Enemy/PlayerDamager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using _Project.Scripts.Domain.Entities.HealthSystem;
+using UnityEngine;
+
+namespace _Project.Scripts.Application.UseCases.Enemy
+{
+    public class PlayerDamager : MonoBehaviour
+    {
+        private Coroutine _coroutine;
+
+        [SerializeField] private int _damage;
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!other.gameObject.TryGetComponent(out PlayerTag player))
+                return;
+
+            _coroutine = StartCoroutine(StartDamaging(player.GetComponent<IDamageable>()));
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            if (_coroutine != null)
+                StopCoroutine(_coroutine);
+        }
+
+        private IEnumerator StartDamaging(IDamageable damageable)
+        {
+            var seconds = new WaitForSeconds(1f);
+            while (true)
+            {
+                damageable.TakeDamage(_damage);
+                yield return seconds;
+            }
+        }
+    }
+}
