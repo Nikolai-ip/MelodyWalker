249c6bd9802011989679d8a96f418dfe29befca5
N[i]kolasPolas <78462900+Nikolai-ip@users.noreply.github.com>
2025-10-06 23:34:13 +0300

    Fix: spell effects

---
+++ Assets/_Project/Scripts/Application/UseCases/Player/PlayerAnimationController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Application.UseCases.Player
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayerAnimationController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rb;
+        private Animator _animator;
+
+        private static readonly int WalkForward = Animator.StringToHash("WalkForward");
+        private static readonly int WalkBackward = Animator.StringToHash("WalkBackward");
+        private static readonly int WalkLeft = Animator.StringToHash("WalkLeft");
+        private static readonly int WalkRight = Animator.StringToHash("WalkRight");
+
+        [SerializeField] private float idleThreshold = 0.1f;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            Vector3 velocity = _rb.linearVelocity;
+
+            // Если стоим
+            if (velocity.magnitude < idleThreshold)
+            {
+                ResetAnimations();
+                return;
+            }
+
+            // Получаем направление относительно персонажа
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity).normalized;
+
+            // Определяем доминирующее направление
+            if (Mathf.Abs(localVelocity.x) > Mathf.Abs(localVelocity.z))
+            {
+                if (localVelocity.x > 0)
+                    PlayAnimation(WalkRight);
+                else
+                    PlayAnimation(WalkLeft);
+            }
+            else
+            {
+                if (localVelocity.z > 0)
+                    PlayAnimation(WalkForward);
+                else
+                    PlayAnimation(WalkBackward);
+            }
+        }
+
+        private void PlayAnimation(int animHash)
+        {
+            ResetAnimations();
+            _animator.SetBool(animHash, true);
+        }
+
+        private void ResetAnimations()
+        {
+            _animator.SetBool(WalkForward, false);
+            _animator.SetBool(WalkBackward, false);
+            _animator.SetBool(WalkLeft, false);
+            _animator.SetBool(WalkRight, false);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Presentation/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using _Project.Scripts.Domain.Entities.HealthSystem;
+using UnityEngine;
+
+namespace _Project.Scripts.Presentation
+{
+    public class HealthBar: MonoBehaviour
+    {
+        [SerializeField] private Health _health;
+        private IDisposable _sub;
+        private void OnEnable()
+        {
+            _sub = _health.CurrentHealth.Subscribe(SetUI);
+        }
+
+        private void SetUI(int hp)
+        {
+            
+        }
+    }
+}
