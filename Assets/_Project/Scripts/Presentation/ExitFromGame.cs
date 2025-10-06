7e187cd72d9c3feecf52ccbaf73be01f52dfb150
HRkiller <killer1234790@gmail.com>
2025-10-07 00:24:03 +0300

    Add: added main menu

---
+++ Assets/_Project/Scripts/Presentation/ExitFromGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Presentation
+{
+    public class ExitFromGame : MonoBehaviour
+    {
+        public void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Presentation/StartGameScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Presentation
+{
+    public class StartGameScene : MonoBehaviour
+    {
+        public void LoadGameScene() => SceneManager.LoadScene("UIDevScene");
+    }
+}
